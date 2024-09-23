8f4cb10eafe99d9ee17f3f548601291d9f565289
Prince Mohammed Huzaifa <lovelyheart1153@gmail.com>
2024-09-23 06:57:25 -0700

    Display genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,28 @@
 import useGenres from "@/hooks/useGenres";
+import getCroppedImage from "@/services/imageUrl";
+import { Button } from "./ui/button";
 
 const GenreList = () => {
   const { data: genres, error, loading } = useGenres();
 
   return (
-    <div>
+    <div className="p-4">
+      <h2 className="text-3xl font-bold mb-4">Genres</h2>
+
       {genres.map((genre) => (
-        <p>{genre.name}</p>
+        <div key={genre.id} className="flex items-center">
+          <img
+            src={getCroppedImage(genre.image_background)}
+            alt="Image of genre"
+            className="size-6 object-cover rounded"
+          />
+          <Button
+            variant="link"
+            className="font-semibold font-lg text-muted-foreground active:text-foreground active:font-bold"
+          >
+            {genre.name} ({genre.games_count})
+          </Button>
+        </div>
       ))}
     </div>
   );
