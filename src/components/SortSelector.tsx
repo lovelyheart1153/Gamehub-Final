d61cae66a8312fde0286e166d5b55dadc01faca1
Prince Mohammed Huzaifa <lovelyheart1153@gmail.com>
2024-09-27 23:06:55 -0700

    Setup react-query

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,3 +1,12 @@
+const sortOrders = [
+  { label: "Relevance", value: "" },
+  { label: "Date Added", value: "-added" },
+  { label: "Name", value: "name" },
+  { label: "Release Date", value: "-released" },
+  { label: "Popularity", value: "-metacritic" },
+  { label: "Average Rating", value: "-rating" },
+];
+
 const SortSelector = () => {
   return <div>SortSelector</div>;
 };
