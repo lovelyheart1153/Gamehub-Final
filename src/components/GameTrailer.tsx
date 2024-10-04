0e781eeb19ba409877da8e53145039b70be262cf
Prince Mohammed Huzaifa <lovelyheart1153@gmail.com>
2024-10-03 17:57:15 -0700

    Layout the game detail page

--- src/components/GameTrailer.tsx
+++ src/components/GameTrailer.tsx
@@ -9,7 +9,12 @@ const GameTrailer = ({ gameId }: Props) => {
   const first = data?.results[0];
 
   return (
-    <video src={first?.data[480]} poster={first?.preview} controls></video>
+    <video
+      className="mb-4"
+      src={first?.data[480]}
+      poster={first?.preview}
+      controls
+    ></video>
   );
 };
 
--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -5,52 +5,52 @@ import GameDescription from "@/components/GameDescription";
 import GameTrailer from "@/components/GameTrailer";
 import ScreenShots from "@/components/ScreenShots";
 import useGame from "@/hooks/useGame";
-// import useTrailers from "@/hooks/useTrailers";
 import { useParams } from "react-router-dom";
 
 const GameDetailPage = () => {
   const params = useParams();
-
   const { data: game, isLoading } = useGame(params.slug!);
-  // const { data } = useTrailers(parseInt(params.gameId!));
-  // console.log(data?.results);
 
   if (!game || isLoading) return null;
 
   return (
-    <div className="pt-[70px] px-4 max-w-[1400px] mx-auto">
+    <div className="pt-[70px] px-4 max-w-[1400px] mx-auto grid sm:grid-cols-2 gap-4">
       <div className="">
-        <h2 className="text-4xl font-bold mb-2">{game?.name}</h2>
-        <GameDescription description={game?.description_raw} />
+        <div className="mb-8">
+          <h2 className="text-2xl sm:text-4xl font-bold mb-2">{game?.name}</h2>
+          <GameDescription description={game?.description_raw} />
+        </div>
+
+        <div className="grid sm:grid-cols-2 gap-y-8">
+          <GameAttributes title="Platforms">
+            {game.parent_platforms.map(({ platform }) => (
+              <Entry key={platform.id}>{platform.name}</Entry>
+            ))}
+          </GameAttributes>
+
+          <GameAttributes title="Metascore">
+            <CriticScoreBadge score={game.metacritic} />
+          </GameAttributes>
+
+          <GameAttributes title="Genres">
+            {game.genres.map((genre) => (
+              <Entry key={genre.id}>{genre.name}</Entry>
+            ))}
+          </GameAttributes>
+
+          <GameAttributes title="Publishers">
+            {game.publishers.map((publisher) => (
+              <Entry key={publisher.id}>{publisher.name}</Entry>
+            ))}
+          </GameAttributes>
+        </div>
       </div>
 
-      <div className="grid sm:grid-cols-2 gap-y-8">
-        <GameAttributes title="Platforms">
-          {game.parent_platforms.map(({ platform }) => (
-            <Entry key={platform.id}>{platform.name}</Entry>
-          ))}
-        </GameAttributes>
+      <div className="">
+        <GameTrailer gameId={parseInt(params.gameId!)} />
 
-        <GameAttributes title="Metascore">
-          <CriticScoreBadge score={game.metacritic} />
-        </GameAttributes>
-
-        <GameAttributes title="Genres">
-          {game.genres.map((genre) => (
-            <Entry key={genre.id}>{genre.name}</Entry>
-          ))}
-        </GameAttributes>
-
-        <GameAttributes title="Publishers">
-          {game.publishers.map((publisher) => (
-            <Entry key={publisher.id}>{publisher.name}</Entry>
-          ))}
-        </GameAttributes>
+        <ScreenShots gameId={parseInt(params.gameId!)} />
       </div>
-
-      <GameTrailer gameId={parseInt(params.gameId!)} />
-
-      <ScreenShots gameId={parseInt(params.gameId!)} />
     </div>
   );
 };
